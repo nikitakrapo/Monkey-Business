9a206913195163d0bd2e71f45c3ae0f880d5e317
nikitakrapo <nikitakrapo@yandex-team.ru>
2022-08-28 14:43:51 +0500

    Add buildSrc with basic dependencies

---
+++ buildSrc/src/main/kotlin/com/nikitakrapo/build/Libraries.kt
@@ -0,0 +1,20 @@
+package com.nikitakrapo.build
+
+object KotlinLib {
+    const val serialization = "org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}"
+    const val datetime = "org.jetbrains.kotlinx:kotlinx-datetime:${Versions.datetime}"
+}
+
+object CoroutinesLib {
+    const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
+}
+
+object KtorLib {
+    const val core = "io.ktor:ktor-client-core:${Versions.ktor}"
+    const val okHttp = "io.ktor:ktor-client-android:${Versions.ktor}"
+    const val darwin = "io.ktor:ktor-client-darwin:${Versions.ktor}"
+}
+
+object LogLib {
+    const val napier = "io.github.aakira:napier:${Versions.napier}"
+}
---
+++ buildSrc/src/main/kotlin/com/nikitakrapo/build/Versions.kt
@@ -0,0 +1,11 @@
+package com.nikitakrapo.build
+
+object Versions {
+    const val kotlin = "1.7.10"
+    const val coroutines = "1.6.4"
+    const val datetime = "0.4.0"
+
+    const val ktor = "2.1.0"
+
+    const val napier = "2.6.1"
+}
