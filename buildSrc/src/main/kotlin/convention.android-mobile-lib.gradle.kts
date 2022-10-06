b5cae3923af37d88de032bb525bebdaea40f93e3
nikitakrapo <nikitakrapo@yandex-team.ru>
2022-10-07 00:21:37 +0200

    Add basic m3 generated theme

---
+++ buildSrc/src/main/kotlin/convention.android-mobile-lib.gradle.kts
@@ -0,0 +1,8 @@
+/**
+ * Convention for android library modules
+ */
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("convention.android-mobile-base")
+}
---
+++ design/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("convention.android-mobile-lib")
+    id("convention.compose")
+}
+
+dependencies {
+    implementation(libs.compose.ui)
+    implementation(libs.compose.material3)
+}
