becf3098416bf0824b373b5047a733dc37949465
nikitakrapo <nikitakrapo@yandex-team.ru>
2022-11-01 19:21:55 +0100

    Add preview to CoreScreen.kt

--- core/src/androidMain/kotlin/com/nikitakrapo/monkeybusiness/CoreScreen.kt
+++ core/src/androidMain/kotlin/com/nikitakrapo/monkeybusiness/CoreScreen.kt
@@ -4,13 +4,16 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.nikitakrapo.monkeybusiness.design.BottomNavigationBar
 import com.nikitakrapo.monkeybusiness.design.NavigationBarItemModel
+import com.nikitakrapo.monkeybusiness.design.theme.MonkeyTheme
 
 @Composable
 fun CoreScreen(
@@ -70,6 +73,38 @@ fun CoreScreen(
                     windowInsets = WindowInsets.navigationBars
                 )
             }
+        }
+    }
+}
+
+@Preview(
+    widthDp = 360,
+    heightDp = 720,
+)
+@Composable
+fun CoreScreen_Preview_Home() {
+    MonkeyTheme {
+        Surface {
+            CoreScreen(
+                modifier = Modifier.fillMaxSize(),
+                component = CoreComponentImpl(initialScreen = CoreComponentImpl.CoreScreen.HOME),
+            )
+        }
+    }
+}
+
+@Preview(
+    widthDp = 360,
+    heightDp = 720,
+)
+@Composable
+fun CoreScreen_Preview_Profile() {
+    MonkeyTheme {
+        Surface {
+            CoreScreen(
+                modifier = Modifier.fillMaxSize(),
+                component = CoreComponentImpl(initialScreen = CoreComponentImpl.CoreScreen.PROFILE),
+            )
         }
     }
 }
