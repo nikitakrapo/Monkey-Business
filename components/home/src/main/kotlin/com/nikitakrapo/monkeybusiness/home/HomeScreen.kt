365ca61e97320a060da358b2ad44d6777876ed90
nikitakrapo <nikitakrapo@yandex-team.ru>
2022-11-02 15:57:16 +0100

    Moved BalanceCard.kt

--- components/home/src/main/kotlin/com/nikitakrapo/monkeybusiness/home/HomeScreen.kt
+++ components/home/src/main/kotlin/com/nikitakrapo/monkeybusiness/home/HomeScreen.kt
@@ -1,28 +1,17 @@
 package com.nikitakrapo.monkeybusiness.home
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nikitakrapo.monkeybusiness.design.theme.MonkeyTheme
+import com.nikitakrapo.monkeybusiness.home.balance.BalanceCard
 
 @Composable
 fun HomeScreen(
@@ -30,52 +19,17 @@ fun HomeScreen(
     component: HomeComponent,
 ) {
     val state = component.state.collectAsState()
-    val balance = state.value.balance
 
     Column(
         modifier = modifier
             .fillMaxWidth()
             .padding(16.dp),
     ) {
-        ElevatedCard(
-            modifier = Modifier.fillMaxWidth(),
-            shape = MaterialTheme.shapes.medium,
-        ) {
-            Column(modifier = Modifier.padding(16.dp)) {
-                Row(
-                    verticalAlignment = Alignment.Bottom,
-                ) {
-                    Text(
-                        modifier = Modifier.alignByBaseline(),
-                        text = balance.amount.toString(),
-                        style = typography.titleLarge,
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text(
-                        modifier = Modifier.alignByBaseline(),
-                        text = balance.currency.name,
-                        style = typography.titleMedium,
-                    )
-                }
-                Spacer(modifier = Modifier.height(12.dp))
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(10.dp),
-                ) {
-                    FilledTonalButton(onClick = component::onTopupClicked) {
-                        Text(
-                            text = stringResource(R.string.topup),
-                            style = typography.bodyLarge,
-                        )
-                    }
-                    FilledTonalButton(onClick = component::onWithdrawClicked) {
-                        Text(
-                            text = stringResource(R.string.withdraw),
-                            style = typography.bodyLarge,
-                        )
-                    }
-                }
-            }
-        }
+        BalanceCard(
+            balance = state.value.balance,
+            onTopupClicked = component::onTopupClicked,
+            onWithdrawClicked = component::onWithdrawClicked,
+        )
     }
 }
 
