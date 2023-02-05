d8198343605778f276579b4245964a9f77f9422b
nikitakrapo <nikitakrapo@yandex-team.ru>
2023-02-06 02:51:47 +0500

    Fix Authorization header passing

--- features/network/src/commonMain/kotlin/com/nikitakrapo/monkeybusiness/network/HttpClientFactory.kt
+++ features/network/src/commonMain/kotlin/com/nikitakrapo/monkeybusiness/network/HttpClientFactory.kt
@@ -1,18 +1,17 @@
 package com.nikitakrapo.monkeybusiness.network
 
-import com.nikitakrapo.monkeybusiness.network.auth.BearerTokensProvider
 import com.nikitakrapo.monkeybusiness.network.auth.ClientAuth
-import io.ktor.client.HttpClientConfig
 import io.ktor.client.plugins.HttpTimeout
-import io.ktor.client.plugins.auth.Auth
-import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
+import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.runBlocking
 import kotlin.time.Duration.Companion.seconds
 
 object HttpClientFactory {
@@ -33,28 +32,12 @@ object HttpClientFactory {
             json()
         }
 
-        installAuth(clientAuth)
-
         defaultRequest {
             url(baseUrl)
+            if (clientAuth is ClientAuth.Bearer) {
+                val token = runBlocking { clientAuth.bearerTokensProvider.getTokens()?.accessToken }
+                token?.let { header(HttpHeaders.Authorization, "Bearer $it") }
+            }
         }
-    }
-
-    private fun HttpClientConfig<*>.installAuth(
-        clientAuth: ClientAuth,
-    ) = install(Auth) {
-        when (clientAuth) {
-            is ClientAuth.Bearer -> installBearerAuth(
-                bearerTokensProvider = clientAuth.bearerTokensProvider,
-            )
-            ClientAuth.None -> {}
-        }
-    }
-
-    private fun Auth.installBearerAuth(
-        bearerTokensProvider: BearerTokensProvider,
-    ) = bearer {
-        loadTokens(bearerTokensProvider::getTokens)
-        refreshTokens { bearerTokensProvider.refreshToken(oldTokens) }
     }
 }
