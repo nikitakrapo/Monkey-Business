8d636829dec9e63b59de2d9ea3987f5da096129e
nikitakrapo <nikitakrapo@yandex-team.ru>
2023-02-12 12:36:30 +0300

    Cut email before @ letter

--- features/account/core/src/commonMain/kotlin/com/nikitakrapo/account/models/AccountDisplayName.kt
+++ features/account/core/src/commonMain/kotlin/com/nikitakrapo/account/models/AccountDisplayName.kt
@@ -3,5 +3,5 @@ package com.nikitakrapo.account.models
 fun Account.getDisplayName(): String {
     username?.let { return it }
     val atCharIndex = email.indexOf(char = '@')
-    return if (atCharIndex != -1) email.substring(0..atCharIndex) else email
+    return if (atCharIndex != -1) email.substring(0 until atCharIndex) else email
 }
