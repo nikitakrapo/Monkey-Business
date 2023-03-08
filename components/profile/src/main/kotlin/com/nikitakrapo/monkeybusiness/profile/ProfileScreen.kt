1de8cfbfc65944151bfaa61e0961dd1d925f62ea
nikitakrapo <nikitakrapo@yandex-team.ru>
2023-03-08 16:43:26 +0300

    Made button ListItem

--- components/profile/src/main/kotlin/com/nikitakrapo/monkeybusiness/profile/ProfileScreen.kt
+++ components/profile/src/main/kotlin/com/nikitakrapo/monkeybusiness/profile/ProfileScreen.kt
@@ -1,28 +1,28 @@
 package com.nikitakrapo.monkeybusiness.profile
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nikitakrapo.monkeybusiness.design.theme.MonkeyTheme
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(
     modifier: Modifier = Modifier,
@@ -31,26 +31,26 @@ fun ProfileScreen(
     val state by component.state.collectAsState()
 
     Column(
-        modifier = modifier
-            .padding(12.dp),
+        modifier = modifier,
     ) {
         ProfileHeader(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .padding(12.dp),
             displayName = state.displayName,
             profileImageUrl = state.profileImageUrl,
             onEditClick = component::onEditClicked,
         )
-        Spacer(modifier = Modifier.height(12.dp))
-        FilledTonalButton(onClick = component::onLogoutClicked) {
-            Icon(
-                modifier = Modifier.size(ButtonDefaults.IconSize),
-                imageVector = Icons.Default.ExitToApp,
-                contentDescription = null,
-            )
-            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
-            Text("Logout")
-        }
+        ListItem(
+            modifier = Modifier
+                .clickable(onClick = component::onLogoutClicked),
+            headlineText = {
+                Text(text = stringResource(R.string.logout))
+            },
+            leadingContent = {
+                Icon(imageVector = Icons.Filled.ExitToApp, contentDescription = null)
+            }
+        )
     }
 }
 
