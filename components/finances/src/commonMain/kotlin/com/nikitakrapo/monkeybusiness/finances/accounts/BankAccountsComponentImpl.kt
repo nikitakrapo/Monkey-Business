54e100d3c4b13642680a6b8768bf6a3b4614bc5e
nikitakrapo <nikitakrapo@yandex-team.ru>
2023-03-11 12:56:26 +0300

    Add fake bank accounts data

--- components/finances/src/commonMain/kotlin/com/nikitakrapo/monkeybusiness/finances/accounts/BankAccountsComponentImpl.kt
+++ components/finances/src/commonMain/kotlin/com/nikitakrapo/monkeybusiness/finances/accounts/BankAccountsComponentImpl.kt
@@ -12,10 +12,24 @@ class BankAccountsComponentImpl(
     private val productOpeningLandingRouter = dependencies.productOpeningLandingRouter
 
     override val state: StateFlow<BankAccountsComponent.State> =
-        MutableStateFlow(BankAccountsComponent.State(emptyList()))
+        MutableStateFlow(BankAccountsComponent.State(fakeBankAccountList()))
 
-    override fun onAccountClicked(index: Int) {
-    }
+    private fun fakeBankAccountList() = listOf(
+        BankAccountViewState(
+            name = "Main account",
+            moneyText = "872 102,00 $",
+            currencyText = "$",
+            bankCardList = fakeSmallBankCardViewState(),
+        )
+    )
+
+    private fun fakeSmallBankCardViewState() = listOf(
+        SmallBankCardViewState("9924"),
+        SmallBankCardViewState("4092"),
+        SmallBankCardViewState("1928"),
+    )
+
+    override fun onAccountClicked(index: Int) {}
 
     override fun onOpenProductClicked() {
         productOpeningLandingRouter.openProductOpening()
