12fa7c541ae4cd42dffa98f4b567095d5e8c0e50
nikitakrapo <nikitakrapo@yandex-team.ru>
2023-05-06 15:54:26 +0200

    Started making BankAccountDetailsScreen

---
+++ components/finances/src/androidMain/kotlin/com/nikitakrapo/monkeybusiness/finances/accounts/details/BankAccountDetailsScreen.kt
@@ -0,0 +1,129 @@
+package com.nikitakrapo.monkeybusiness.finances.accounts.details
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nikitakrapo.monkeybusiness.design.theme.MonkeyTheme
+import com.nikitakrapo.monkeybusiness.finances.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BankAccountDetailsScreen(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        TopAppBar(
+            title = {},
+            navigationIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Outlined.ArrowBack,
+                        contentDescription = stringResource(R.string.cd_navigate_back)
+                    )
+                }
+            }
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp)
+        ) {
+            Text(
+                text = "Bank account",
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.W500
+            )
+            Text(
+                text = "1 544,50 $",
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.W500
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(MaterialTheme.shapes.small)
+                    .background(MaterialTheme.colorScheme.primaryContainer),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                    modifier = Modifier
+                        .width(96.dp)
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text("Add")
+                }
+                Column(
+                    modifier = Modifier
+                        .width(96.dp)
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Icon(imageVector = Icons.Filled.ArrowForward, contentDescription = null)
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text("Transfer")
+                }
+                Column(
+                    modifier = Modifier
+                        .width(96.dp)
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Icon(imageVector = Icons.Filled.MoreVert, contentDescription = null)
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text("More")
+                }
+            }
+        }
+    }
+}
+
+@Preview(
+    widthDp = 360,
+    heightDp = 720
+)
+@Preview(
+    widthDp = 360,
+    heightDp = 720, uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Composable
+fun BankAccountDetailsScreen_Preview() {
+    MonkeyTheme {
+        Surface {
+            BankAccountDetailsScreen()
+        }
+    }
+}
